6aac90efef138e26573c18988ba60f8b2d98ef29
nuodun1235646 <27979831@qq.com>
2025-11-24 21:17:08 +0800

    给每笔交易维护一个trade id

--- data_server/binance/ws_binance/utils/binance_pos_analysis.py
+++ data_server/binance/ws_binance/utils/binance_pos_analysis.py
@@ -1,4 +1,5 @@
 from data_server.binance.ws_binance.utils.reids_connect import RedisClient
+import uuid
 
 previous_data = None
 
@@ -34,7 +35,24 @@ class BinanceAnalysisService:
                 p['pnl_ratio'] = ratio
         return positions
 
+    def apply_trade_ids(self, old_data, new_data):
+        old_iter = old_data if isinstance(old_data, list) else []
+        old_index = {(i.get('symbol'), i.get('positionSide')): i.get('trade_id') for i in old_iter}
+        if isinstance(new_data, list):
+            for p in new_data:
+                key = (p.get('symbol'), p.get('positionSide'))
+                tid = old_index.get(key)
+                if not tid:
+                    tid = uuid.uuid4().hex
+                p['trade_id'] = tid
+        return new_data
+
     def get_old_data(self):
+        global previous_data
+        if previous_data is None:
+            cached = self.redis_client.get_json("positions:binance")
+            if cached is not None:
+                previous_data = cached
         return previous_data
 
     def set_old_data(self, new_data):
@@ -89,6 +107,7 @@ class BinanceAnalysisService:
                         'new_position_amt': new_amt,
                         'old_pnl_ratio': old_pnl_ratio,
                         'new_pnl_ratio': new_pnl_ratio,
+                        'trade_id': j.get('trade_id'),
                         'change': change
                     })
         print("修改：", results)
@@ -99,11 +118,14 @@ class BinanceAnalysisService:
         new_data = self.add_pnl_ratio(new_data)
         old_data = self.get_old_data()
 
-        # if old_data is None:
-        #     self.set_old_data(new_data)
-        #     return
+        if old_data is None:
+            new_data = self.apply_trade_ids([], new_data)
+            self.set_old_data(new_data)
+            self.redis_client.set_json("positions:binance", new_data)
+            return
 
         if new_data != old_data:
+            new_data = self.apply_trade_ids(old_data, new_data)
             self.set_old_data(new_data)
             self.redis_client.set_json("positions:binance", new_data)
 
@@ -117,16 +139,27 @@ class BinanceAnalysisService:
                 for item in removed_items:
                     self.redis_client.conn.srem("symbol:binance", f"{item.get('symbol')}")
                     print("存入数据库：", item)
+
+            changed_items = self.find_changed_items(old_data, new_data)
+            if changed_items:
+                for item in changed_items:
+                    print("变化的数据：", item)
         return
 
 
 if __name__ == "__main__":
-    new_data = []
-    old_data = [{'symbol': '1000PEPEUSDT', 'positionSide': 'LONG', 'positionAmt': '852', 'unrealizedProfit': '0.00930384', 'isolatedMargin': '0', 'notional': '5.01923424', 'isolatedWallet': '0', 'initialMargin': '0.50192343', 'maintMargin': '0.03262502', 'updateTime': 1763031017638}]
-    new_data = [{'symbol': '1000PEPEUSDT', 'positionSide': 'LONG', 'positionAmt': '851', 'unrealizedProfit': '0.00930384', 'isolatedMargin': '0', 'notional': '5.01923424', 'isolatedWallet': '0', 'initialMargin': '0.50192343', 'maintMargin': '0.03262502', 'updateTime': 1763031017638}]
+    # old_data = []
+    old_data = [
+        {'symbol': '1000PEPEUSDT', 'positionSide': 'LONG', 'positionAmt': '851', 'unrealizedProfit': '0.00930384',
+         'isolatedMargin': '0', 'notional': '5.01923424', 'isolatedWallet': '0', 'initialMargin': '0.50192343',
+         'maintMargin': '0.03262502', 'updateTime': 1763031017638, 'trade_id': '2e317d6f9ace4114a4c44624eff952f5'}]
+    new_data = [
+        {'symbol': '1000PEPEUSDT', 'positionSide': 'LONG', 'positionAmt': '852', 'unrealizedProfit': '0.00930384',
+         'isolatedMargin': '0', 'notional': '5.01923424', 'isolatedWallet': '0', 'initialMargin': '0.50192343',
+         'maintMargin': '0.03262502', 'updateTime': 1763031017638, 'trade_id': '2e317d6f9ace4114a4c44624eff952f5'}]
     obj = BinanceAnalysisService()
     obj.set_old_data(old_data)
-    # obj.analysis(new_data)
+    obj.analysis(new_data)
     # obj.find_removed_items(old_data, new_data)
     # obj.find_added_items(old_data, new_data)
-    obj.find_changed_items(old_data, new_data)
+    # obj.find_changed_items(old_data, new_data)
