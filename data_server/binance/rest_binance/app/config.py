e834f940a66438863fe82e410eedd7616564ce29
nuodun1235646 <27979831@qq.com>
2025-11-26 00:11:33 +0800

    完善爬虫数据

--- data_server/binance/rest_binance/app/config.py
+++ data_server/binance/rest_binance/app/config.py
@@ -6,16 +6,13 @@ class Settings:
     redis_password: str = os.environ.get('REDIS_PASSWORD', None),
     redis_port: int = int(os.environ.get('REDIS_PORT', 6379))
     redis_db: int = int(os.environ.get('REDIS_BD', 0))
-    api_rate_limit_per_second: int = 20
-    rate_limits: dict = {
-        'open_interest': 20,
-        'funding': 20,
-    }
-    kline_rate_limits_seconds: dict = {
+    rate_limits_seconds: dict = {
         '1m': 60,
         '30m': 1800,
         '1h': 3600,
-        '2h': 7200,
+        '4h': 14400,
+        '12h': 43200,
+        '1d': 86400,
     }
     http_timeout_s: int = 10
     log_level: str = "INFO"
--- data_server/binance/rest_binance/app/fetchers.py
+++ data_server/binance/rest_binance/app/fetchers.py
@@ -11,44 +11,159 @@ except ImportError:
     from utils import logger
     from config import settings
 
-KLINE_LIMITERS = {k: TokenBucket(rate=1 / v, capacity=1) for k, v in settings.kline_rate_limits_seconds.items()}
-OPEN_INTEREST_LIMITER = TokenBucket(rate=1 / settings.rate_limits['open_interest'], capacity=1)
-FUNDING_RATE_LIMITER = TokenBucket(rate=1 / settings.rate_limits['funding'], capacity=1)
+LIMITERS = {k: TokenBucket(rate=1 / v, capacity=1) for k, v in settings.rate_limits_seconds.items()}
 
 BASE_URL = 'https://fapi.binance.com'
 
 
 async def fetch_kline(symbol: str, interval: str, limit: int = 200):
+    """K线数据"""
     url = BASE_URL + '/fapi/v1/klines'
     params = {
         'symbol': symbol,
         'interval': interval,
         'limit': limit
     }
-    limiter = KLINE_LIMITERS.get(interval)
+    limiter = LIMITERS.get(interval)
     async with limiter:
         data = await http_client.request("GET", url, params=params)
         print(data)
 
 
-async def kline_poller(symbol: str, interval: str, limit: int = 200):
+async def fetch_topLongShortAccountRatio(symbol: str, period: str):
+    """大户账户数多空比"""
+    url = BASE_URL + '/futures/data/topLongShortAccountRatio'
+    if period == '1m':
+        period = '5m'
+    params = {
+        'symbol': symbol,
+        'period': period,
+        'limit': 30
+    }
+    limiter = LIMITERS.get(period)
+    async with limiter:
+        res = await http_client.request("GET", url, params=params)
+        # todo 首次获取数据整体添加，后续获取做增量更新
+        print(res)
+
+
+async def fetch_topLongShortPositionRatio(symbol: str, period: str):
+    """大户持仓多空比"""
+    url = BASE_URL + '/futures/data/topLongShortPositionRatio'
+    if period == '1m':
+        period = '5m'
+    params = {
+        'symbol': symbol,
+        'period': period,
+        'limit': 30
+    }
+    limiter = LIMITERS.get(period)
+    async with limiter:
+        res = await http_client.request("GET", url, params=params)
+        # todo 首次获取数据整体添加，后续获取做增量更新
+        print(res)
+
+
+async def fetch_globalLongShortAccountRatio(symbol: str, period: str):
+    """全局账户多空比"""
+    url = BASE_URL + '/futures/data/globalLongShortAccountRatio'
+    if period == '1m':
+        period = '5m'
+    params = {
+        'symbol': symbol,
+        'period': period,
+        'limit': 30
+    }
+    limiter = LIMITERS.get(period)
+    async with limiter:
+        res = await http_client.request("GET", url, params=params)
+        # todo 首次获取数据整体添加，后续获取做增量更新
+        print(res)
+
+
+async def fetch_takerlongshortRatio(symbol: str, period: str):
+    """ 合约主动买卖量 """
+    url = BASE_URL + '/futures/data/takerlongshortRatio'
+    if period == '1m':
+        period = '5m'
+    params = {
+        'symbol': symbol,
+        'period': period,
+        'limit': 30
+    }
+    limiter = LIMITERS.get(period)
+    async with limiter:
+        res = await http_client.request("GET", url, params=params)
+        # todo 首次获取数据整体添加，后续获取做增量更新
+        print(res)
+
+
+async def fetch_ticker24hr(symbol: str):
+    """24hr价格变动情况"""
+    url = BASE_URL + '/fapi/v1/ticker/24hr'
+    params = {
+        'symbol': symbol
+    }
+    limiter = LIMITERS.get('1h')
+    async with limiter:
+        res = await http_client.request("GET", url, params=params)
+        priceChangePercent = res.get('priceChangePercent')
+        volume = res.get('volume')
+        quoteVolume = res.get('quoteVolume')
+        highPrice = res.get('highPrice')
+        lowPrice = res.get('lowPrice')
+        data = {
+            'priceChangePercent': priceChangePercent,
+            'volume': volume,
+            'quoteVolume': quoteVolume,
+            'highPrice': highPrice,
+            'lowPrice': lowPrice
+        }
+        print(data)
+
+
+async def fetch_fundingRate(symbol: str):
+    """获取资金费率"""
+    url = BASE_URL + '/fapi/v1/fundingRate'
+    params = {
+        'symbol': symbol,
+        'limit': 200
+    }
+    limiter = LIMITERS.get('4h')
+    async with limiter:
+        res = await http_client.request("GET", url, params=params)
+        # todo 首次获取数据整体添加，后续获取做增量更新
+        print(res)
+
+
+async def spider_poller(symbol: str, interval: str, limit: int = 200):
     while True:
         try:
             await fetch_kline(symbol, interval, limit)
+            await fetch_takerlongshortRatio(symbol, interval)
+            await fetch_topLongShortAccountRatio(symbol, interval)
+            await fetch_topLongShortPositionRatio(symbol, interval)
+            await fetch_globalLongShortAccountRatio(symbol, interval)
         except Exception as e:
             logger.error("kline_poller_error interval=%s %s", interval, e)
+
 
 async def _main():
     try:
         tasks = [
-            asyncio.create_task(kline_poller("BTCUSDT", "1m", 200)),
-            asyncio.create_task(kline_poller("BTCUSDT", "30m", 200)),
-            asyncio.create_task(kline_poller("BTCUSDT", "1h", 200)),
-            asyncio.create_task(kline_poller("BTCUSDT", "2h", 200)),
+            asyncio.create_task(spider_poller("BTCUSDT", "1m", 200)),
+            asyncio.create_task(spider_poller("BTCUSDT", "30m", 200)),
+            asyncio.create_task(spider_poller("BTCUSDT", "1h", 200)),
+            asyncio.create_task(spider_poller("BTCUSDT", "4h", 200)),
+            asyncio.create_task(spider_poller("BTCUSDT", "12h", 200)),
+            asyncio.create_task(spider_poller("BTCUSDT", "1d", 200)),
+            await fetch_ticker24hr("BTCUSDT"),
+            await fetch_fundingRate("BTCUSDT")
         ]
         await asyncio.gather(*tasks)
     finally:
         await http_client.close()
+
 
 if __name__ == "__main__":
     asyncio.run(_main())
