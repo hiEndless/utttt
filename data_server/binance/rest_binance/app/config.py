df3c5c7ca0a377ee5b3a2b0f0cbf6456c32344a6
nuodun1235646 <27979831@qq.com>
2025-11-26 00:34:26 +0800

    修改配置

--- data_server/binance/rest_binance/app/config.py
+++ data_server/binance/rest_binance/app/config.py
@@ -5,7 +5,7 @@ class Settings:
     redis_host: str = os.environ.get('REDIS_HOST', '127.0.0.1')
     redis_password: str = os.environ.get('REDIS_PASSWORD', None),
     redis_port: int = int(os.environ.get('REDIS_PORT', 6379))
-    redis_db: int = int(os.environ.get('REDIS_BD', 0))
+    redis_db: int = int(os.environ.get('REDIS_BD', 1))
     rate_limits_seconds: dict = {
         '1m': 60,
         '30m': 1800,
@@ -16,9 +16,6 @@ class Settings:
     }
     http_timeout_s: int = 10
     log_level: str = "INFO"
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
