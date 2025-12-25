a13bec0de414c6b45b65b88eae965cd9a7d46d3c
nuodun1235646 <27979831@qq.com>
2025-12-25 16:29:03 +0800

    开启本机代理

--- data_server/binance/rest_binance/app/main.py
+++ data_server/binance/rest_binance/app/main.py
@@ -24,10 +24,11 @@ def make_spider(interval: str, limit: int = 300):
         logger.info("task_trigger name=%s interval=%s time=%s", f"spider_{interval}", interval,
                     time.strftime("%Y-%m-%d %H:%M:%S"))
         await fetch_kline(symbol, interval, limit)
-        await fetch_takerLongShortRatio(symbol, interval)
-        await fetch_topLongShortAccountRatio(symbol, interval)
-        await fetch_topLongShortPositionRatio(symbol, interval)
-        await fetch_globalLongShortAccountRatio(symbol, interval)
+        if interval != "1m":
+            await fetch_takerLongShortRatio(symbol, interval)
+            await fetch_topLongShortAccountRatio(symbol, interval)
+            await fetch_topLongShortPositionRatio(symbol, interval)
+            await fetch_globalLongShortAccountRatio(symbol, interval)
 
     return run
 
