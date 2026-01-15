a797e4458e05a72e354815a061b9c92f28181781
nuodun1235646 <27979831@qq.com>
2026-01-15 20:09:37 +0800

    更新

--- agent_server/configs/source.py
+++ agent_server/configs/source.py
@@ -66,6 +66,18 @@ def query_db_env():
             "llm_api_key": "sk-kfbnznycbjvseqxfqbthkytcwquklptyastuhzjcdutnvbfa",
             "a2a_url": "http://localhost:10007/",
         },
+        "event_summary": {
+            "model_id": "Qwen/Qwen3-VL-235B-A22B-Instruct",
+            "llm_base_url": "https://api.siliconflow.cn/v1",
+            "llm_api_key": "sk-kfbnznycbjvseqxfqbthkytcwquklptyastuhzjcdutnvbfa",
+            "a2a_url": "http://localhost:10007/",
+        },
+        "trade_summary": {
+            "model_id": "Qwen/Qwen3-VL-235B-A22B-Instruct",
+            "llm_base_url": "https://api.siliconflow.cn/v1",
+            "llm_api_key": "sk-kfbnznycbjvseqxfqbthkytcwquklptyastuhzjcdutnvbfa",
+            "a2a_url": "http://localhost:10007/",
+        },
     }
 
 
