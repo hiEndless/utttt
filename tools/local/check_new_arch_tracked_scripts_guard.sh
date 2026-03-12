6308e5bde4c36e4e934ec5f7949b6184906bb955
nuodun1235646 <27979831@qq.com>
2026-03-12 22:27:20 +0800

    chore(guards): track release-ready script in key-script guard

--- tools/local/check_new_arch_tracked_scripts_guard.sh
+++ tools/local/check_new_arch_tracked_scripts_guard.sh
@@ -11,6 +11,7 @@ REQUIRED_SCRIPTS=(
   "tools/local/check_market_state_contract_entry_guard.sh"
   "tools/local/check_event_center_contract_entry_guard.sh"
   "tools/local/check_release_baseline_alignment.sh"
+  "tools/local/check_release_ready.sh"
   "tools/local/check_runner_output_schema_guard.sh"
   "tools/local/integration_smoke_new_arch.sh"
 )
